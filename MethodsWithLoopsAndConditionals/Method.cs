9a87856eedcae58b26c465b0615865c82c1b498b
Yun Jang <jang7j@yahoo.com>
2022-08-15 11:23:02 -0500

    initial commit

---
+++ MethodsWithLoopsAndConditionals/Method.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsWithLoopsAndConditionals
+{
+    internal class Method
+    {
+
+        public static void Thousands()
+        {
+            for (int i = 1000; i >= -1000; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void ThreeToNineNineNine()
+        {
+            for (int i = 3; i <= 999; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static bool isEqual(int a, int b)
+        {
+            var message = (a == b) ? true : false;
+            return message;
+        }
+        public static void EvenOdd()
+        {
+            var evenOrOdd = int.Parse(Console.ReadLine());
+            var result = (evenOrOdd % 2 == 0) ? "Even" : "Odd";
+            Console.WriteLine(result);
+        }
+        public static void PositiveOrNegative()
+        {
+            var check = int.Parse(Console.ReadLine());
+            if (check >= 0)
+            {
+                Console.WriteLine("Number is positive");
+            }
+            else
+            {
+                Console.WriteLine("Number is negative");
+            }
+
+        }
+        public static void canVote()
+        {
+            Console.WriteLine("Enter your age : ");
+            var age = int.Parse(Console.ReadLine());
+            bool validNumber = int.TryParse(Console.ReadLine(), out int input);
+            if (validNumber == true)
+            {
+                if (input >= 18)
+                {
+                    Console.WriteLine("You are eligible to vote");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry you cannot vote");
+                }
+            }
+
+            Console.WriteLine("Invalid input");
+
+
+
+        }
+
+    }
+}
+
---
+++ MethodsWithLoopsAndConditionals/Program.cs
@@ -0,0 +1,21 @@
+﻿using MethodsWithLoopsAndConditionals;
+
+//Method 1: Print to the console all numbers 1000 through -1000   
+Method.Thousands();
+
+//Method 2: Print to the console numbers 3 through 999 by 3 each time
+
+Method.ThreeToNineNineNine();
+
+//Method 3: Accept two integers as parameterss and check whether they are equal or not
+Console.WriteLine(Method.isEqual(2, 8));
+
+
+//Method 4: Check whether a given number is even or odd
+Method.EvenOdd();
+
+//Method 6: Check whether a given number is positive or negative
+Method.PositiveOrNegative();
+
+//Method 7: Read the age of a candidate and determine whether they can vote
+Method.canVote();
